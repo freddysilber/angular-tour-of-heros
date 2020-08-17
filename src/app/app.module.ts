6301577811cb4c3279859a0a8676ada39789a4ed
freddysilber <freddy.silber@gmail.com>
2020-08-17 12:13:53 -0600

    fixing names

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HerosComponent } from './heros/heros.component';
+import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component'
@@ -12,7 +12,7 @@ import { DashboardComponent } from './dashboard/dashboard.component'
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent,
+    HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent
